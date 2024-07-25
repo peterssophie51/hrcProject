9b7a0482b51cbd39d055e9e208e632c712a4cb38
Sophie Peters <peterssophie51@gmail.com>
2024-07-25 14:36:28 +1200

    create the home page

--- App.js
+++ App.js
@@ -1,13 +1,26 @@
 import './gesture-handler'; //always has to be at the top
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native'
+
+function HomeScreen( {navagation}) {
+  return (
+    <View>
+      <Text>Home Page</Text>
+    </View>
+  )
+}
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
 

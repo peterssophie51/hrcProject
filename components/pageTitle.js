462787ad9b62c57278cd6212cadfeee29ded3355
Sophie Peters <peterssophie51@gmail.com>
2024-07-28 14:33:08 +1200

    working nav bar

--- components/pageTitle.js
+++ components/pageTitle.js
@@ -1,8 +1,10 @@
 import { Text } from 'react-native';
 
+
+
 export function PageTitle(props) {
     return (
-        <Text style={{ textAlign:'center', fontSize:40, fontWeight: 'bold', marginTop: '3%'}}>{props.title}</Text>
+        <Text style={{ textAlign:'center', fontSize:40, fontWeight: 'bold', marginTop: '3%',  }}>{props.title}</Text>
     )
 }
 

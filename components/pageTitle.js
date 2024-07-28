3d66693a15dfe5e22eba1a44847236e67e992701
Sophie Peters <peterssophie51@gmail.com>
2024-07-28 12:45:25 +1200

    adding page title

--- components/pageTitle.js
+++ components/pageTitle.js
@@ -2,6 +2,7 @@ import { Text } from 'react-native';
 
 export function PageTitle(props) {
     return (
-        <Text style={{ textAlign:'center', fontSize:40, fontWeight: 'bold', marginTop: '3%', fontFamily: 'Arial'}}>{props.title}</Text>
+        <Text style={{ textAlign:'center', fontSize:40, fontWeight: 'bold', marginTop: '3%'}}>{props.title}</Text>
     )
 }
+

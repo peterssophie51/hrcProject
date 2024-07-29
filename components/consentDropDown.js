d083546a4507c55cc26952ebb480206d6528967b
Sophie Peters <peterssophie51@gmail.com>
2024-07-30 08:25:17 +1200

    create accordian drop down

--- components/consentDropDown.js
+++ components/consentDropDown.js
@@ -1,29 +1,15 @@
-import DropDownPicker from 'react-native-dropdown-picker';
-import { useState } from 'react';
+import * as React from 'react';
+import { List } from 'react-native-paper';
 
 export function ConsentDropdownHeader() {
-  const [open, setOpen] = useState(false);
-  const [value, setValue] = useState('Farm Water Consent');
-  const [items, setItems] = useState([
-    {label: 'Farm Water Consent', value: 'Farm Water Consent'},
-    {label: 'Crops Water Consent', value: 'Crops Water Consent'},
-    {label: 'Animals Water Consent', value: 'Animals Water Consent'}
-  ]);
 
   return (
-    <DropDownPicker
-      open={open}
-      value={value}
-      items={items}
-      setOpen={setOpen}
-      setValue={setValue}
-      setItems={setItems}
-      listItemContainerStyle={{}}
-      style={{}}
-      labelStyle={{}}
+      <List.Accordion title="Uncontrolled Accordion">
+        <List.Item title="First item" />
+        <List.Item title="Second item" />
+      </List.Accordion>
 
-    />
-  );
+ 
+  )
 }
-
 

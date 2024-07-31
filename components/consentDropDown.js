7910238eeb198e95ff44e226859deff351de7aab
Sophie Peters <peterssophie51@gmail.com>
2024-07-31 13:52:55 +1200

    allow users to change the nickname of consents

--- components/consentDropDown.js
+++ components/consentDropDown.js
@@ -4,29 +4,33 @@ import { Pressable } from 'react-native';
 import { Image, View } from 'react-native';
 import { ConsentDropdownItem } from './consentDropdownItem';
 
+import { useState } from 'react';
+
 export function ConsentDropdownHeader() {
   const [currentConsent, setcurrentConsent] = React.useState("Farm Water Consent")
   const [currentConsentATH, setcurrentConsentATH] = React.useState("ATH-2002009085")
+
   return (
+    <View>
       <List.Accordion title={currentConsent} description={currentConsentATH}>
         <ConsentDropdownItem 
-          title="Farm Water Consent" 
           description="ATH-2002009085" 
           setcurrentConsent={setcurrentConsent} 
           setcurrentConsentATH={setcurrentConsentATH}/>
         <ConsentDropdownItem 
-          title="Crops Water Consent" 
           description="ATH-2002008648" 
           setcurrentConsent={setcurrentConsent} 
           setcurrentConsentATH={setcurrentConsentATH}/>
         <ConsentDropdownItem 
-          title="Animals Water Consent" 
           description="ATH-2002009348" 
           setcurrentConsent={setcurrentConsent} 
           setcurrentConsentATH={setcurrentConsentATH}/>
         <List.Item title="+ Add New Consent" />
       </List.Accordion>
+    </View>
   )
 }
+
+
 
 
--- components/consentDropdownItem.js
+++ components/consentDropdownItem.js
@@ -1,19 +1,48 @@
 import * as React from 'react';
-import { Image, View } from 'react-native';
+import { Image, View, Pressable, TextInput, Button, TouchableOpacity } from 'react-native';
 import { List, } from 'react-native-paper';
+import { useState } from 'react';
+
 
 
 export function ConsentDropdownItem(props) {
+    const [visible, setVisible] = useState(false);
+    const [currentName, setcurrentName] = useState(props.consentNickname)
+    const [consentNickname, setconsentNickname]= useState("Nickname")
+
+    const toggleVisibility = () => {
+        setVisible(!visible);
+      };
+
+    
+      
+
     return(
+        <View>
         <List.Item 
-        title={props.title} 
+        title={consentNickname}
         description={props.description} 
-        onPress={() => {props.setcurrentConsent(props.title); props.setcurrentConsentATH(props.description)}} 
+        onPress={() => {props.setcurrentConsent(consentNickname); props.setcurrentConsentATH(props.description)}} 
         right={() => 
             <View style={{display:'flex', flexDirection:'row'}}>
-                <Image source={require('../images/editBlack.png')} style={{height:25, width:25, marginRight:20}}/>
+                <TouchableOpacity onPress={toggleVisibility}>
+                    <Image source={require('../images/editBlack.png')} style={{height:25, width:25, marginRight:20}}/>
+                </TouchableOpacity>
                 <Image source={require('../images/crossBlack.png')} style={{height:25, width:25}}/>
-            </View>}/>
+            </View>}
+        />
+        <View>
+            {visible && (
+                <>
+                    <TextInput style={{ height: 50, width: 100, backgroundColor: 'pink' }} onChangeText={setcurrentName} />
+                    <Button title="Submit" onPress={() => {toggleVisibility(); setconsentNickname(currentName)}}/>
+                    <Button title="Cancel" onPress={toggleVisibility} />
+                </>
+    )}
+    </View>
+        </View>
     )
 }
+
+
 

a546689bf790f1a8b0288b43dcf3227382e1e4a2
Sophie Peters <peterssophie51@gmail.com>
2024-08-08 19:10:26 +1200

    river flow card

---
+++ components/river/riverFlow.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { View } from "react-native";
+import { CalibriBoldText } from "../fonts/calibriBoldFont";
+import { StyleSheet } from "react-native";
+import { Dimensions } from "react-native";
+
+export function RiverFlowTitle() {
+    const riverFlow = 45.7 // river flow value
+
+    return(
+        <View style={styles.container}>
+            <View style={styles.titleContainer}>
+                <CalibriBoldText title="RIVER" style={styles.title}/>
+                <CalibriBoldText title="FLOW" style={styles.title}/>
+            </View>
+            <View style={styles.value}>
+                <CalibriBoldText title={riverFlow} style={styles.flow}/>
+                    <CalibriBoldText style={styles.units} title='M'/>
+                        <CalibriBoldText style={styles.superscript} title='3'/>
+                    <CalibriBoldText style={styles.units} title='/S'/>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        marginTop: Dimensions.get('window').width * 0.05, 
+        width: Dimensions.get('window').width * 0.9,
+        height:Dimensions.get('window').height * 0.12, 
+        marginLeft:Dimensions.get('window').width *0.05, 
+        marginRight:Dimensions.get('window').width * 0.08,
+        borderRadius: 20,
+        backgroundColor: '#eeeeee',
+        display: 'flex', 
+        flexDirection: 'row'
+    },
+    titleContainer: {
+        marginTop: Dimensions.get('window').height * 0.014,
+        marginLeft: Dimensions.get('window').width * 0.05
+    },
+    title: {
+        fontSize: 25,
+    },
+    flow: {
+        fontSize:45,
+        marginLeft: Dimensions.get('window').width * 0.04
+    },
+    superscript: {
+        fontSize:20,
+        textAlign:'center',
+        lineHeight:50,
+        fontFamily:'Calibri'
+    },
+    units: {
+        fontSize:30,
+        lineHeight:70,
+        fontFamily:'Calibri'
+    },
+    value: {
+        display:'flex', 
+        flexDirection:'row',
+        marginTop: Dimensions.get('window').width * 0.04,
+        marginLeft: Dimensions.get('window').width * 0.25
+    }
+})
--- pages/riverPage.js
+++ pages/riverPage.js
@@ -2,14 +2,16 @@ import { View } from 'react-native';
 import { CalibriBoldText } from '../components/fonts/calibriBoldFont.js';
 import { Dimensions, StyleSheet } from 'react-native';
 import { CalibriText } from '../components/fonts/calibriFont.js';
+import { RiverFlowTitle } from '../components/river/riverFlow.js';
 
 export function RiverPage ({ navagation }) {
   const flowsite = "Rangitikei at Mangaweka"
     return (
-      <View>
-        < CalibriBoldText style={styles.title} title="River" />
-        < CalibriBoldText  style={styles.flowsite} title={flowsite} />
-        < CalibriText style={styles.timeRecorded} title='Last Recorded at 20:00 (NZST) June 14th 2024'/>
+      <View  style={{width:Dimensions.get('window').width, height:Dimensions.get('window').height * 2, backgroundColor:'white'}}>
+        <CalibriBoldText style={styles.title} title="River" />
+        <CalibriBoldText  style={styles.flowsite} title={flowsite} />
+        <CalibriText style={styles.timeRecorded} title='Last Recorded at 20:00 (NZST) June 14th 2024'/>
+        <RiverFlowTitle />
       </View>
     )
   }

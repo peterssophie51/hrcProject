1438131fc68f3e83e9d7c88a16192f872a88a370
Sophie Peters <peterssophie51@gmail.com>
2024-08-08 18:44:53 +1200

    adding titles to river page

--- pages/riverPage.js
+++ pages/riverPage.js
@@ -1,12 +1,35 @@
 import { View } from 'react-native';
 import { CalibriBoldText } from '../components/fonts/calibriBoldFont.js';
+import { Dimensions, StyleSheet } from 'react-native';
+import { CalibriText } from '../components/fonts/calibriFont.js';
 
 export function RiverPage ({ navagation }) {
+  const flowsite = "Rangitikei at Mangaweka"
     return (
       <View>
-        < CalibriBoldText  style={{textAlign:'center', fontSize:40, marginTop:'15%'}} title="River" />
+        < CalibriBoldText style={styles.title} title="River" />
+        < CalibriBoldText  style={styles.flowsite} title={flowsite} />
+        < CalibriText style={styles.timeRecorded} title='Last Recorded at 20:00 (NZST) June 14th 2024'/>
       </View>
     )
   }
 
+const styles = StyleSheet.create({
+  title: {
+    textAlign:'center', 
+    fontSize:40, 
+    marginTop: Dimensions.get('window').height * 0.12
+  },
+  flowsite: {
+    textAlign: 'center', 
+    fontSize: 30, 
+    marginTop: Dimensions.get('window').height * 0.01
+  },
+  timeRecorded: {
+    textAlign: 'center',
+    fontSize: 18,
+    marginTop: Dimensions.get('window').height * 0.01
+  }
+
+  })
   

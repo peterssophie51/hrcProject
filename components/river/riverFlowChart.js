1ec9ffef2a7f53cc97d11521a640b69e8c17809c
Sophie Peters <peterssophie51@gmail.com>
2024-08-10 20:33:47 +1200

    working clickable datapoints

--- components/river/riverFlowChart.js
+++ components/river/riverFlowChart.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { LineChart } from "react-native-gifted-charts";
-import { Dimensions, View, StyleSheet } from "react-native";
+import { Dimensions, View, StyleSheet, Text, Alert } from "react-native";
 
 export function RiverFlowChart(props) {
     const data = props.currentData
@@ -8,19 +8,33 @@ export function RiverFlowChart(props) {
     return(
         <View style={styles.container}>
             <LineChart 
+                style={{zIndex:0}}
                 data = {data} 
                 width={Dimensions.get('window').width * 0.75} 
                 height={Dimensions.get('window').height * 0.35}
                 maxValue={Math.max(...data.map(item => item.value)) * 1.1}
                 noOfSections={12}
-                initialSpacing={Dimensions.get('window').width * 0.02}
+                initialSpacing={0}
                 yAxisTextStyle={styles.axis}
                 xAxisLabelTexts={props.currentLabels}
                 xAxisLabelTextStyle={styles.axis}
                 color={'#00A7CF'}
                 thickness={3}
+                showVerticalLines
+                showValuesAsDataPointsText
+
                 dataPointsColor="#00A7CF"
-                dataPointsRadius={0}
+                focusEnabled
+                textShiftY={-10}
+                textShiftX={-10}
+                textColor="black"
+                textFontSize={12}
+                focusedDataPointColor={'#00A7CF'}
+                focusedDataPointRadius={5}
+                showTextOnFocus={true}
+                showDataPointLabelOnFocus
+                showDataPointOnFocus
+                delayBeforeUnFocus={5000}
             />
         </View>
         

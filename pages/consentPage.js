3bcd12c74573d1087a62db60d9ab1e480904f726
Sophie Peters <peterssophie51@gmail.com>
2024-08-11 08:34:29 +1200

    compliance card

--- pages/consentPage.js
+++ pages/consentPage.js
@@ -3,13 +3,14 @@ import { CalibriBoldText } from '../components/fonts/calibriBoldFont.js';
 import { CalibriText } from '../components/fonts/calibriFont.js';
 import { StyleSheet, Dimensions } from 'react-native';
 import { ConsentInfo } from '../components/consents/consentInfo.js';
-import { ColorSpace } from 'react-native-reanimated';
+import { ComplianceCard } from '../components/consents/complianceCard.js';
 
 export function ConsentPage ({ navagation }) {
     return (
       <View style={{width:Dimensions.get('window').width, height:Dimensions.get('window').height * 2, backgroundColor:'white'}}>
         <CalibriBoldText  style={styles.title} title="Consent" />
         <ConsentInfo consentExpiration="June 25th 2028" consentFlowSite="Rangitikei at Mangaweka" annualMax="No recorded data"/>
+        <ComplianceCard />
       </View>
     )
   }

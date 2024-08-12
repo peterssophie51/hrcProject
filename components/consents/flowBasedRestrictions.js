19a29d5e9582e46cdb192581a17e029ac964f6aa
Sophie Peters <peterssophie51@gmail.com>
2024-08-12 13:15:38 +1200

    calling current flow based restriction in accordion drop down

--- components/consents/flowBasedRestrictions.js
+++ components/consents/flowBasedRestrictions.js
@@ -2,11 +2,19 @@ import React from "react";
 import { Text, View } from "react-native";
 import { List } from "react-native-paper";
 import { StyleSheet, Dimensions } from "react-native";
+import { RestrictionInfo } from "./restrictionInfo";
 
 export function FlowbasedRestriction() {
     return (
         <List.Accordion style={styles.container}
-        left={() => <Text style={styles.currentText}>Current flowbased restriction component goes here</Text>} >
+        left={() => <RestrictionInfo restrictionTitle={'CURRENT FLOW BASED RESTRICTION: '} restriction={'NONE'}
+        data={{
+          flowAtRestriction: 45, 
+          instaneous: 34, 
+          hourlyRestriction: 28, 
+          annualRestriction: 2800, 
+          dailyRestriction: 40
+        }}/>} >
         <List.Item left={ () => 
             <View>
                 <Text style={styles.droppedText}>Other flowbased restriction</Text>
@@ -24,14 +32,14 @@ const styles = StyleSheet.create({
     container:{
         marginTop: Dimensions.get('window').width * 0.04, 
         width: Dimensions.get('window').width * 0.9,
-        height:Dimensions.get('window').height * 0.2, 
+        height:Dimensions.get('window').height * 0.4, 
         marginLeft:Dimensions.get('window').width *0.05, 
         marginRight:Dimensions.get('window').width * 0.08,
         borderRadius: 20,
         backgroundColor: '#eeeeee',
     },
     dropDown: {
-        marginTop: Dimensions.get('window').width * -0.1,
+        marginTop: Dimensions.get('window').width * -0.05,
         backgroundColor: '#eeeeee',
         marginLeft: Dimensions.get('window').width *0.05, 
         width: Dimensions.get('window').width * 0.9,
--- pages/faqPage.js
+++ pages/faqPage.js
@@ -5,17 +5,7 @@ import { RestrictionInfo } from '../components/consents/restrictionInfo.js';
 export function FAQPage ({ navagation }) {
     return (
       <View style={{width:Dimensions.get('window').width, height:Dimensions.get('window').height * 2, backgroundColor:'white', flex:1}}>
-        < CalibriBoldText  style={styles.title} title="FAQ" />
-        <View style={styles.container}>
-          <RestrictionInfo restrictionTitle={'CURRENT FLOW BASED RESTRICTION: '} restriction={'NONE'}
-          data={{
-            flowAtRestriction: 45, 
-            instaneous: 34, 
-            hourlyRestriction: 28, 
-            annualRestriction: 2800, 
-            dailyRestriction: 40
-          }}/>
-        </View>
+
       </View>
     )
   }

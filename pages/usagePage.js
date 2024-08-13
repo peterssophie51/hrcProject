d31c1ffcc3d2e4f5742cb7344f15ef74962a232f
Sophie Peters <peterssophie51@gmail.com>
2024-08-13 21:53:19 +1200

    setting up page design

--- pages/usagePage.js
+++ pages/usagePage.js
@@ -1,12 +1,18 @@
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { CalibriBoldText } from '../components/fonts/calibriBoldFont';
 
 export function UsagePage ({ navagation }) {
     return (
-      <View>
+      <View style={styles.page}>
         < CalibriBoldText  style={{textAlign:'center', fontSize:40, marginTop:'15%'}} title="Usage" />
       </View>
     )
   }
 
-  
+const styles = StyleSheet.create({
+  page: {
+    width:Dimensions.get('window').width, 
+    height:Dimensions.get('window').height * 2, 
+    backgroundColor:'white'
+  }
+})

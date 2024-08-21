1ceca1336394f7c84873f4e3bd9fb3e9ed3e2c07
Sophie Peters <peterssophie51@gmail.com>
2024-08-22 08:30:20 +1200

    adding in horizons logo image

--- pages/faqPage.js
+++ pages/faqPage.js
@@ -1,10 +1,23 @@
-import { View, StyleSheet, Dimensions, ScrollView } from 'react-native';
+import { View, StyleSheet, Dimensions, ScrollView, Image } from 'react-native';
 import { CalibriBoldText } from '../components/fonts/calibriBoldFont.js';
 import { ContactCard } from '../components/faq/contactCard.js';
 import { FAQCard } from '../components/faq/faqCard.js';
 
 export function FAQPage ({ navagation }) {
   const contacts = ['Horizons website', 'Horizons phone number', 'Horizons fax', 'Horizons email']
+  const faq = [
+    {   
+      key: 0, 
+      question:'How do I find my Consent ATH?', 
+      answer:"You should already have your consent ATH when applyig for a consent, if you don't have this infrormation, please contact us at Horizons!"
+    },
+    {
+      key: 1,
+      question:'What is my consent flowsite?',
+      answer:'This is the section of the river that is linked to your abstraction. For more information, you can visit out website or contact us at Horizons!'
+    }
+  
+  ]
     return (
       <View style={{width:Dimensions.get('window').width, height:Dimensions.get('window').height * 2, backgroundColor:'white', flex:1}}>
         <CalibriBoldText style={styles.title} title="FAQ" />
@@ -17,7 +30,12 @@ export function FAQPage ({ navagation }) {
           })
           }
           <CalibriBoldText style={styles.subtitle} title="FAQ"/>
-            <FAQCard question='Question' answer='Answer'/>
+            {faq.map((item, index) => {
+              return(
+                <FAQCard key={item.key} question={item.question} answer={item.answer}/>
+              )
+            })}
+          <Image source={require('../images/horizonsFlowTransperant.png')} style={styles.image}/>
         </ScrollView>
       </View>
     )
@@ -42,5 +60,11 @@ export function FAQPage ({ navagation }) {
       fontSize: 30,
       color: '#72BF44',
       marginTop: Dimensions.get('window').height * 0.03
-    }
+    },
+    image: {
+      marginTop: Dimensions.get('window').width * 0.01, 
+      marginLeft: Dimensions.get('window').width * 0.12, 
+      width: Dimensions.get('window').width * 0.75,
+      height: Dimensions.get('window').width * 0.25,
+  }
   })

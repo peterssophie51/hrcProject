b13d0e4a345b619d44b51fd2f07928e1a71653bf
Sophie Peters <peterssophie51@gmail.com>
2024-08-21 12:13:46 +1200

    add subtitles

--- pages/faqPage.js
+++ pages/faqPage.js
@@ -5,7 +5,9 @@ import { RestrictionInfo } from '../components/consents/restrictionInfo.js';
 export function FAQPage ({ navagation }) {
     return (
       <View style={{width:Dimensions.get('window').width, height:Dimensions.get('window').height * 2, backgroundColor:'white', flex:1}}>
-
+        <CalibriBoldText style={styles.title} title="FAQ" />
+        <CalibriBoldText style={styles.subtitle} title="Contacts"/>
+        <CalibriBoldText style={styles.subtitle} title="FAQ"/>
       </View>
     )
   }
@@ -23,5 +25,11 @@ export function FAQPage ({ navagation }) {
       height: Dimensions.get('window').height * 0.42,
       marginLeft: Dimensions.get('window').width * 0.05,
       marginTop: Dimensions.get('window').width * 0.05
+    },
+    subtitle: {
+      textAlign: 'center',
+      fontSize: 30,
+      color: '#72BF44',
+      marginTop: Dimensions.get('window').height * 0.03
     }
   })

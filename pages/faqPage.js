97d48143fff41649ae00bd961c9a6e29c3c193f6
Sophie Peters <peterssophie51@gmail.com>
2024-09-04 13:08:17 +1200

    commenting faq page

--- pages/faqPage.js
+++ pages/faqPage.js
@@ -1,8 +1,10 @@
 import { View, StyleSheet, Dimensions, ScrollView, Image } from 'react-native';
+//import components
 import { CalibriBoldText } from '../components/fonts/calibriBoldFont.js';
 import { ContactCard } from '../components/faq/contactCard.js';
 import { FAQCard } from '../components/faq/faqCard.js';
 
+//component for the faq page
 export function FAQPage ({ navagation }) {
   const contacts = [
     {
@@ -29,8 +31,7 @@ export function FAQPage ({ navagation }) {
       method:'mailto',
       link: 'help@horizons.govt.nz'
     }
-  ]
-  
+  ] //all contacts to be rendered in contacts card
   const faq = [
     {   
       key: 0, 
@@ -43,25 +44,30 @@ export function FAQPage ({ navagation }) {
       answer:'This is the section of the river that is linked to your abstraction. For more information, you can visit out website or contact us at Horizons!'
     }
   
-  ]
+  ] //all faq questions and answers to be rendered in faq cards
 
     return (
       <View style={{width:Dimensions.get('window').width, height:Dimensions.get('window').height * 2, backgroundColor:'white', flex:1}}>
+        {/*page title*/}
         <CalibriBoldText style={styles.title} title="FAQ" />
+        {/*making page scrollable*/}
         <ScrollView>
+          {/*subheading for contacts info*/}
           <CalibriBoldText style={styles.subtitle} title="Contacts"/>
-          {contacts.map((item, index) => {
+          {contacts.map((item, index) => { //map through list of contacts to render a card for each list item
             return (
               <ContactCard key={item.key} title={item.name} method={item.method} link={item.link}/>
             )
           })
           }
+          {/*subheading for faq info*/}
           <CalibriBoldText style={styles.subtitle} title="FAQ"/>
-            {faq.map((item, index) => {
+            {faq.map((item, index) => { //map through lsit of faq to render a card for each list item
               return(
                 <FAQCard key={item.key} question={item.question} answer={item.answer}/>
               )
             })}
+          {/*horizons logo at the bottom of the page*/}
           <Image source={require('../images/horizonsFlowTransperant.png')} style={styles.image}/>
         </ScrollView>
       </View>
@@ -69,12 +75,12 @@ export function FAQPage ({ navagation }) {
   }
 
   const styles = StyleSheet.create({
-    title: {
+    title: { //style page title
       textAlign:'center', 
       fontSize:40, 
       marginTop: Dimensions.get('window').height * 0.12,
     },
-    container: {
+    container: { //style page container
       backgroundColor: '#eeeeee',
       borderRadius: 20,
       width: Dimensions.get('window').width * 0.9,
@@ -82,13 +88,13 @@ export function FAQPage ({ navagation }) {
       marginLeft: Dimensions.get('window').width * 0.05,
       marginTop: Dimensions.get('window').width * 0.05
     },
-    subtitle: {
+    subtitle: { //style subheadings in page
       textAlign: 'center',
       fontSize: 30,
       color: '#72BF44',
       marginTop: Dimensions.get('window').height * 0.03
     },
-    image: {
+    image: { //style bottom horizons image
       marginTop: Dimensions.get('window').width * 0.01, 
       marginLeft: Dimensions.get('window').width * 0.12, 
       width: Dimensions.get('window').width * 0.75,

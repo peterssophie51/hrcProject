6d074d0346a9ac6fb9da03311516965213ab22ac
Sophie Peters <peterssophie51@gmail.com>
2024-10-01 21:04:39 +1300

    comparison radio buttons

--- components/reports/comparisonsContent.js
+++ components/reports/comparisonsContent.js
@@ -1,12 +1,14 @@
 import { View, Text } from "react-native";
 import { ComparisonsGraph } from "./comparisonGraph";
 import { CheckboxCard } from "./checkBoxCard";
+import { ComparisonRadios } from "./comparisonRadios";
 
 export function ComparisonsContent(props) {
     return (
         <View style={{display: 'flex', flexDirection:'column'}}>
             <ComparisonsGraph />
             <CheckboxCard flowmeters={props.flowmeters} />
+            <ComparisonRadios />
         </View>
     )
 }

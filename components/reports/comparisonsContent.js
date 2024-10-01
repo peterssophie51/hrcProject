5a162cdb59ac5fcef21d76824629d959b1547614
Sophie Peters <peterssophie51@gmail.com>
2024-10-01 14:35:45 +1300

    create dropdowns for content

---
+++ components/reports/comparisonsContent.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export function ComparisonsContent() {
+    return (
+        <View>
+            
+        </View>
+    )
+}
--- pages/reportsPage.js
+++ pages/reportsPage.js
@@ -2,19 +2,23 @@ import { View, Text } from 'react-native';
 import { useState } from 'react';
 import { Dimensions, StyleSheet } from 'react-native'
 import { CalibriBoldText } from '../components/fonts/calibriBoldFont';
+import { PageSections } from '../components/reports/pageSections';
 
 export function ReportsPage ({ }) {
     return(
         <View style={styles.page}>
             <CalibriBoldText title="Reports" style={styles.title}/>
+            <PageSections title='COMPARISONS'/>
+            <PageSections title="ALERTS" />
+            <PageSections title='REPORTS' />
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     page: {
-        width: Dimensions.get('window').width,
-        height: Dimensions.get('window').height,
+        width: Dimensions.get('window').width ,
+        height: Dimensions.get('window').height * 2,
         backgroundColor: 'white'
     },
     title: {
